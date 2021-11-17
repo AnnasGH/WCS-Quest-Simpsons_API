d0e84776dc3b200141de845412f5bda7978bd72c
AnnasGH <anna.jaksics@gmail.com>
2021-11-17 17:52:40 +0100

    added conditional character direction styling

--- src/components/DisplaySimpson/index.jsx
+++ src/components/DisplaySimpson/index.jsx
@@ -1,13 +1,18 @@
 import './index.css';
 
 function DisplaySimpson ({simpson}) {
+
     return (
         simpson && (
             <div className="DisplaySimpson">
                 <div id="Title">"The Simpsons"</div>
                 <div className="Simpson">
                     <div id="ImgContainer">
-                    <img src={simpson.image} alt={simpson.character}/>
+                    <img 
+                    src={simpson.image} 
+                    alt={simpson.character} 
+                    style={simpson.characterDirection === "Right" ? {transform: "rotateY(0.5turn"} : null}
+                    />
                     </div>
                     <div id="TextContainer">
                         <div id='SimpsonQuote'>"{simpson.quote}"</div>
